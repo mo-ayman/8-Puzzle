c67107e3fd85327ae619bfa7bb42a183dea34724
mo-ayman <m.ayman.saaed@gmail.com>
2022-10-13 20:06:18 +0200

    Modify goal test function

--- AI/Searcher.py
+++ AI/Searcher.py
@@ -1,9 +1,14 @@
 class Searcher(object):
     def __init__(self):
         self.__visited = None
+        self.final_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
     def search(self, state):
         return None
 
-    def goalTest(self, state):
-        return False
+    def goal_test(self, state):
+        for i in range(3):
+            for j in range(3):
+                if self.final_state[i][j] is not state.grid[i][j]:
+                    return False
+        return True

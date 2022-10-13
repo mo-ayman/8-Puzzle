55b469d91d63c0baa5013dfbcb4479532daaf782
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-10-13 18:33:57 +0200

    Update Searcher

--- AI/Searcher.py
+++ AI/Searcher.py
@@ -1,9 +1,17 @@
-class Searcher(object):
-    def __init__(self):
-        self.__visited = None
+import abc
 
+
+class Searcher(abc.ABC):
+    def __init__(self):
+        self.visited = []
+
+    @abc.abstractmethod
     def search(self, state):
         return None
 
     def goalTest(self, state):
-        return False
+        return state.grid == [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]

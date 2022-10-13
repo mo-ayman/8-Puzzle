17df0037fa640aaea431591df51dba2e45802376
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-10-13 22:47:59 +0200

    Manhhattan typo fixed

--- AI/ManhattanHeuristic.py
+++ AI/ManhattanHeuristic.py
@@ -3,7 +3,7 @@ from .Heuristic import Heuristic
 
 class ManhattanHeuristic(Heuristic):
     def heuristicCostCell(self, curri,currj,number):
-        i,j = self.getGoalPositio(number)
+        i,j = self.getGoalPosition(number)
         return abs(curri-i)+ abs(currj-j)
 
     pass

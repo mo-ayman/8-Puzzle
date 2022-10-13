696672520bfc0eb01b26e0b1bdec6ade16f4d3a3
Gamal Abdul Hameed <es-gamalabdelhameed314@alexu.edu.eg>
2022-10-13 18:34:43 +0200

    Inserted Initial DFS

--- AI/DFS.py
+++ AI/DFS.py
@@ -1,6 +1,21 @@
-from .Searcher import Searcher
+from Searcher import Searcher
 
 
 class DFS(Searcher):
     def __init__(self):
-        self.__frontier = None
+        super().__init__()
+        self.frontier = []
+
+    def search(self, state):
+        self.frontier.append(state)
+        while len(self.frontier) > 0:
+            state = self.frontier.pop()
+            if state not in self.visited:
+                self.visited.append(state)
+
+            if self.goalTest(state):
+                return state
+            for neighbour in state.get_successor():
+                if neighbour not in self.frontier and neighbour not in self.visited:
+                    self.frontier.append(neighbour)
+        return False

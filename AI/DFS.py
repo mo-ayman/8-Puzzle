6718e9618d059aa0e6b5ca15aeeba998b704bd4f
mo-ayman <m.ayman.saaed@gmail.com>
2022-10-13 20:23:12 +0200

    Add enhanced DFS

--- AI/DFS.py
+++ AI/DFS.py
@@ -1,6 +1,25 @@
 from .Searcher import Searcher
+from queue import LifoQueue
 
 
 class DFS(Searcher):
     def __init__(self):
-        self.__frontier = None
+        super().__init__()
+        self.__frontier = LifoQueue()
+        self.__visited = set()
+
+    def search(self, initialState):
+        self.__frontier.put(initialState)
+
+        while not self.__frontier.empty():
+            state = self.__frontier.get()
+
+            if super().goal_test(state):
+                return state
+
+            for neighbor in state.get_successor():
+                grid_hash = neighbor.get_hash()
+                if grid_hash not in self.__visited:
+                    self.__visited.add(grid_hash)
+                    self.__frontier.put(neighbor)
+        return None

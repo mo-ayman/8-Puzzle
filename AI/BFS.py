49e4043165743cdd9d7c9f52ff02898447f2b219
Gamal-Abd-El-Hameed <es-gamalabdelhameed314@alex.edu.eg>
2022-10-14 11:27:14 +0200

    Returned DFS & BFS to origin, added initialState to visited

--- AI/BFS.py
+++ AI/BFS.py
@@ -6,10 +6,10 @@ class BFS(Searcher):
     def __init__(self):
         super().__init__()
         self.__frontier = Queue()
-        self.__visited = set()
 
     def search(self, initialState):
         self.__frontier.put(initialState)
+        self.visited.add(initialState.get_hash())
 
         while not self.__frontier.empty():
             state = self.__frontier.get()
@@ -19,7 +19,7 @@ class BFS(Searcher):
 
             for neighbor in state.get_successor():
                 grid_hash = neighbor.get_hash()
-                if grid_hash not in self.__visited:
-                    self.__visited.add(grid_hash)
+                if grid_hash not in self.visited:
+                    self.visited.add(grid_hash)
                     self.__frontier.put(neighbor)
         return None
--- AI/DFS.py
+++ AI/DFS.py
@@ -6,25 +6,22 @@ class DFS(Searcher):
     def __init__(self):
         super().__init__()
         self.__frontier = LifoQueue()
-        self.__frontier_set = set()
 
-    def search(self, initial_state):
-        self.__frontier.put(initial_state)
-        self.__frontier_set.add(initial_state.get_hash())
+    def search(self, initialState):
+        self.__frontier.put(initialState)
+        self.visited.add(initialState.get_hash())
 
         while not self.__frontier.empty():
             state = self.__frontier.get()
-            self.__frontier_set.remove(state.get_hash())
-            self.visited.add(state.get_hash())
 
             if super().goal_test(state):
                 return state
 
             for neighbor in state.get_successor():
                 grid_hash = neighbor.get_hash()
-                if grid_hash not in self.visited and grid_hash not in self.__frontier_set:
+                if grid_hash not in self.visited:
+                    self.visited.add(grid_hash)
                     self.__frontier.put(neighbor)
-                    self.__frontier_set.add(grid_hash)
         return None
 """
 6 7 8

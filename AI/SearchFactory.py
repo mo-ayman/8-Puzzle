84c5c02fa82a3466e46c20337ec3ac8e9f0a96ae
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-10-16 11:25:09 +0200

    enter number corresponds to Search Algorithm
    1.DFS
    2.BFS
    3.AStar Euclidean
    4.AStar Manhattan

--- AI/SearchFactory.py
+++ AI/SearchFactory.py
@@ -4,14 +4,13 @@ from AI import DFS,BFS,AStar,EuclideanHeuristic , ManhattanHeuristic
 class SearchFactory:
     @staticmethod
     def get_search_algo(search_algo):
-        print(search_algo)
-        if search_algo == 'DFS':
+        if search_algo == '1':
             search = DFS()
-        elif search_algo == 'BFS':
+        elif search_algo == '2':
             search = BFS()
-        elif search_algo == 'A* Euclidean':
+        elif search_algo == '3':
             search = AStar(EuclideanHeuristic())
-        elif search_algo == 'A* Manhattan':
+        elif search_algo == '4':
             search = AStar(ManhattanHeuristic())
         return search
 
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def take_input():
     for i in range(3):
         initial_state.grid[i][0], initial_state.grid[i][1], initial_state.grid[i][2] = map(int, input().split())
 
-    print("Enter the search algorithm")
+    print("Enter the number corresponding to search algorithm:\n1.DFS\n2.BFS\n3.AStar Euclidean\n4.AStar Manhattan")
     search_algo = input()
     return search_algo
 

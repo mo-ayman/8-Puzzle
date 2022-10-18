ea2c3376f77a699fcbe454bd052f7ecc8e5c39f2
mo-ayman <m.ayman.saaed@gmail.com>
2022-10-18 11:40:59 +0200

    Change visited in AStar to track explored nodes
    I have changed visited array to track explored node only as in Astar we have to update the frontier which we don't have to in BFS and DFS

--- AI/AStar.py
+++ AI/AStar.py
@@ -11,17 +11,16 @@ class AStar(Searcher):
     def search(self, initialState):
         stateExplored=1
         self.__frontier.put((self.__heuristic.heuristic_cost(initialState)+initialState.previous_cost,stateExplored,initialState))
-        self.__visited.add(initialState.get_hash());
         while not self.__frontier.empty():
             state = self.__frontier.get()[2]
 
+            self.__visited.add(state.get_hash());
             if super().goal_test(state):
                 return state
 
             for neighbor in state.get_successor():
                 grid_hash = neighbor.get_hash()
                 if grid_hash not in self.__visited:
-                    self.__visited.add(grid_hash)
                     stateExplored+=1
                     self.__frontier.put((self.__heuristic.heuristic_cost(neighbor)+neighbor.previous_cost,stateExplored,neighbor))
                 else:
